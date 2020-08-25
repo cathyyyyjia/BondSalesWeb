166b567fd15ca364bcce29b24bfbf322075377c7
cathyyytjia <cathyjia@uw.edu>
2020-08-25 13:57:52 +0800

    add web

---
+++ web/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = '债券销售管理系统';
+  titleEn = 'Bond Sales Management System';
+  active = 'top';
+}
